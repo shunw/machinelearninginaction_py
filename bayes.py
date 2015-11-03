d9cea23ca6aa404430d2d4c367479c5be90bda67
unknown <lishunw@LISHUNW3.auth.hpicorp.net>
2015-11-03 16:54:16 +0800

    add bayes code

---
+++ bayes.py
@@ -0,0 +1,34 @@
+import operator
+
+def loadDataSet():
+	postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'], 
+					['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'], 
+					['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+					['stop', 'posting', 'stupid', 'worthless', 'garbage'], 
+					['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+					['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+	classVec = [0, 1, 0, 1, 0, 1]
+	return postingList, classVec
+
+def createVocabList(dataSet):
+	vocabSet = set([])
+	for document in dataSet:
+		vocabSet = vocabSet | set(document)
+		# print vocabSet
+	# print vocabSet
+	return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+		else:
+			print 'the word: %s is not in my Vocabulary!' % word
+	return returnVec
+
+if __name__ == '__main__':
+	listOPosts, listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	print setOfWords2Vec(myVocabList, listOPosts[3])
+	
