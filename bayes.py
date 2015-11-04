ba18aa8541e6f1f38c021f6e99f7db0d5745b296
unknown <lishunw@LISHUNW3.auth.hpicorp.net>
2015-11-04 12:47:51 +0800

    update the bayes

--- bayes.py
+++ bayes.py
@@ -1,4 +1,5 @@
 import operator
+from numpy import *
 
 def loadDataSet():
 	postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'], 
@@ -27,8 +28,32 @@ def setOfWords2Vec(vocabList, inputSet):
 			print 'the word: %s is not in my Vocabulary!' % word
 	return returnVec
 
+def trainNB0(trainMatrix, trainCategory):
+	numTrainDocs = len(trainMatrix)
+	numWords = len(trainMatrix[0])	#for the training matrix first row's word qty
+	pAbusive = sum(trainCategory)/float(numTrainDocs)	#abusive number/ total line which is total document
+	p0Num = zeros(numWords); p1Num = zeros(numWords)	#make an array with the numwords qty len
+	p0Denom = 0.0; p1Denom = 0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = p1Num/p1Denom
+	p0Vect = p0Num/p0Denom
+	return p0Vect, p1Vect, pAbusive
+
+
 if __name__ == '__main__':
 	listOPosts, listClasses = loadDataSet()
 	myVocabList = createVocabList(listOPosts)
-	print setOfWords2Vec(myVocabList, listOPosts[3])
+	trainMat = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+	
+	p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+	print p1V
+	
 	
